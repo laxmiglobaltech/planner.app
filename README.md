c56075fcc1efbcef07bcf5be6ff1a73aa0d628cc
Ashish Suman <ashishsuman4u@gmail.com>
2024-01-17 19:30:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a planner app which will help the user to become more productive.
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Planner App',
+  description: 'This is a productive application that will help you create yearly, monthly and daily plan.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
